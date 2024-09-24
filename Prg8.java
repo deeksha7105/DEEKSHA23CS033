2fd16146482406571ef7e86745fdbd8b0c63bd79
deeksha7105 <169142780+deeksha7105@users.noreply.github.com>
2024-09-24 15:43:49 +0530

    Human

---
+++ Prg8.java
@@ -0,0 +1,23 @@
+package lab7;
+class Human{
+    public void job(){
+        System.out.println("Working Professional");}}
+class Teacher extends Human{
+    public void job(){
+        System.out.println("Teacher");}}
+class Doctor extends Human{
+    public void job(){
+        System.out.println("Doctor ");}}
+
+public class Prg8 {
+    public static void main(String[] args){
+        Human amar=new Human();
+        Teacher babu = new Teacher();
+        Doctor chandra = new Doctor();
+        Human dinesh = new Doctor();
+        amar.job();
+        babu.job();
+        chandra.job();
+        dinesh.job();
+    }
+}
