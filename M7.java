77ca4577b020d30aa0e9cee82e2760d7dc5fa8ec
deeksha7105 <169142780+deeksha7105@users.noreply.github.com>
2024-11-05 16:20:54 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,9 @@
+
+package pkg12;
+import pkg5.Tools2;
+public class M7 {
+    public static void main(String[] args) {
+        Tools2 t1=new Tools2();
+        System.out.println(t1.reverse("Madan"));
+    }
+}
