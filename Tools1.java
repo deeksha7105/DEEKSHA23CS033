7924283961a961297e4044526877517cf19846b0
deeksha7105 <169142780+deeksha7105@users.noreply.github.com>
2024-11-12 14:23:14 +0530

    Add files via upload

---
+++ Tools1.java
@@ -0,0 +1,14 @@
+package pkg12;
+public class Tools1{
+    String s1="";
+    
+    int len1=0;
+    public String reverse(String s1){
+        String s2="";
+        len1=s1.length();
+        for(int i=0;i<len1;i++){
+           s2=s2+s1.substring(len1-(i+1),len1-i);
+       }
+        return s2;
+    }
+}
