6e24d2ab04b94870d31871673b49659fc7d76a86
deeksha7105 <169142780+deeksha7105@users.noreply.github.com>
2024-12-19 00:52:00 +0530

    lab 10

---
+++ MyPackage.java
@@ -0,0 +1,6 @@
+package mypack;
+public class MyPackage{
+	public void display() {
+		System.out.println("This message is from MyPackage class within the package mypack");
+	}
+}
---
+++ Test_MyPackage.java
@@ -0,0 +1,8 @@
+package pkg;
+import mypack.MyPackage;
+public class Test_MyPackage {
+	public static void main(String[] args) {
+		MyPackage mypack1=new MyPackage();
+		mypack1.display();
+	}
+}
